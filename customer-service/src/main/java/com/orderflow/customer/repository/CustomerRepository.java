bb7388ffd80e43f3da159172b7f8a325c87a5b4c
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2026-07-07 01:01:49 -0300

    feat(customer): adiciona métodos de consulta na CustomerRepository

--- customer-service/src/main/java/com/orderflow/customer/repository/CustomerRepository.java
+++ customer-service/src/main/java/com/orderflow/customer/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.orderflow.customer.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.orderflow.customer.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-    
+    Optional<Customer> findByEmail(String email);
+    boolean existsByEmail(String email);
+    Optional<Customer> findByCpf(String cpf);
+    boolean existsByCpf(String cpf);
 }
